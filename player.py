6dc92603808d81cd7a3a638cc6ad9d588bc32f8b
bthomas218 <bradythomas690@gmail.com>
2025-10-02 15:13:19 -0500

    feat: added player movement

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS, PLAYER_ANGULAR_SPEED
+from constants import PLAYER_RADIUS, PLAYER_ANGULAR_SPEED, PLAYER_SPEED
 from circleshape import CircleShape
 import pygame
 
@@ -21,7 +21,11 @@ class Player(CircleShape):
     
     def rotate(self, dt):
         self.rotation += PLAYER_ANGULAR_SPEED * dt
-    
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -29,3 +33,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)

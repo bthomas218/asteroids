9b22162c9938172144a650c831dd3c95d8e069a6
bthomas218 <bradythomas690@gmail.com>
2025-10-02 15:34:02 -0500

    refactor: added player to updatable and drawable groups

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ from constants import *
 from player import Player
 
 def main():
+    # Game initilization
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
+    # Game Loop
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        player.update(dt)
+        
+        # Update game state
+        updatable.update(dt)
 
         screen.fill("black")
         
-        player.draw(screen)
+        # Draw Game objects
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+
+        # Frame Rate: 60
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":

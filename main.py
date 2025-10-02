438dfe618faea3fcf5a88821ec5248abbffe2b09
bthomas218 <bradythomas690@gmail.com>
2025-10-02 13:26:57 -0500

    feat: added game constants

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":

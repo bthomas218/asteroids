6f9c46f1e8365ac8b8b7e772edef5fc720888137
bthomas218 <bradythomas690@gmail.com>
2025-10-05 07:40:56 -0500

    feat: added bullet firing

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
-from constants import PLAYER_RADIUS, PLAYER_ANGULAR_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_ANGULAR_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 from circleshape import CircleShape
 import pygame
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -37,3 +38,9 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+    
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.math.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
